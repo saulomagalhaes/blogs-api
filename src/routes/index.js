d9748afa2429b04ddc81af38b59d4f8864886fc9
Saulo Magalhães <saulo.magalhaes@outlook.com.br>
2022-07-14 10:16:59 -0300

    feat(user.routes): iniciando a implementação da método post para user.routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const routesLogin = require('./login.routes');
+const routesUser = require('./user.routes');
 
 router.use('/login', routesLogin);
+router.use('/user', routesUser);
 
 module.exports = router;
---
+++ src/routes/user.routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const router = express.Router();
+const userController = require('../controllers/userController');
+
+router.post('', userController.create);
+
+module.exports = router;
